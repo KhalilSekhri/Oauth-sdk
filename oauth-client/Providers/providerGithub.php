8faf7fa501e8145a93bbd5b796bc9ed85ebcaa4c
KhalilSekhri <khalil.sekhri@gmail.com>
2020-07-20 00:00:21 +0200

    Ajout du provider Github

--- oauth-client/Providers/providerGithub.php
+++ oauth-client/Providers/providerGithub.php
@@ -1,48 +1,62 @@
-<?php
+<?
 namespace Providers;
-use Providers\provider;
+use Providers\providerHandler;
 
-class ProviderLocal extends Provider
+class GitHubProvider extends Provider
 {
 
-protected $name = "providerLocal";
-protected $client_id = "client_5edfd43b0db573.88203718";
-protected $client_secret="e0a6a1f5c55fafd48cbcce2b7279d4029fad76f4";
-protected $url = "http://localhost:7070/auth";
-protected $response_type = "code";
-protected $scop = "email";
-protected $state = "DEAZFAEF321432DAEAFD3E13223R";
-//protected $redirectUri = "http://localhost:7071/success";
+    protected $name = "ProviderGitHub";
+    protected $client_id = "0fa761508f7ae7b7fe18";
+    protected $client_secret="cf7843945b8f3406bb7c6683a99afa24bbe90780";
+    protected $url = "https://api.github.com/";
+    protected $scope = "login";
+    protected $state = "github";
+    protected $linkAutorisation = "https://github.com/login/oauth/authorize";
+    protected $linkAccessToken = "https://github.com/login/oauth/access_token";
+    protected $redirect_url = "http://localhost:7071/sucess";
 
-    public function __construct(string $client_id, string $client_secret)
+
+    public function construct(string $client_id, string $client_secret)
     {
-        parent::__construct($client_id, $client_secret);
+        parent::construct();
+        //$this->url= $this->url ."?response_type=code&client_id={$this->client_id}&state={$this->state}&scope={$this->scope}&redirect_uri={$this->redirect_url}";
     }
 
-    public function getProviderLocal() {
+    public function getProviderGitHub() {
 
         ['code' => $code, 'state' => $rstate] = $_GET;
 
         // Check state origin
-        if ($STATE === $rstate) {
-        // Get access token
-        ['token' => $token] = json_decode(file_get_contents($link), true);
+        if ($state === $rstate) {
+            // Get access token
+            //$link = "http://oauth-server/token?grant_type=authorization_code&code=%7B$this-%3Ecode%7D&client_id=%7B$this-%3Eclient_id%7D&client_secret=%7B$this-%3Eclient_secret%7D";
+            $link = [
+                "client_id" => $this->client_id,
+                "client_secret" => $this->client_secret,
+                "code" => $code,
+              ];
 
-        // Get user data
-        $link = "http://oauth-server/me";
-        $rs = curl_init($link);
-        curl_setopt_array($rs, [
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HEADER => 0,
-            CURLOPT_HTTPHEADER => [
-                "Authorization: Bearer {$token}"
-            ]
-        ]);
-        echo curl_exec($rs);
-        curl_close($rs);
-    } else {
-        http_response_code(400);
-        echo "Invalid state";
-    }
-    }
+              ['token' => $token] = json_decode(file_get_contents($link), true);
+
+            // Get user data
+            $link = "http://oauth-server/me";
+            $rs = curl_init($link);
+            curl_setopt_array($rs, [
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HEADER => 0,
+                CURLOPT_HTTPHEADER => [
+                    "Authorization: Bearer {$token}"
+                ]
+            ]);
+
+            $tr = curl_init($rs);
+            echo curl_exec($rs);
+            curl_close($rs);
+        } 
+
+        else {
+            http_response_code(400);
+            echo "Invalid state";
+    } 
+}
 }
